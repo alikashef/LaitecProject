cf4b0703a2066d31ee67ecfa02ad694974cab8b3
ali kashef <37862916+alikashef@users.noreply.github.com>
2020-07-27 23:30:22 +0430

    IntoGit
    Laitec Project in to github

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        margin:0,
+        autoHeight:true,
+        autoplay:false,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true,
+        loop:true,
+        center: true,
+
+
+    });
+});
